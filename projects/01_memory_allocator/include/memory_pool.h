0fa7ab4664c42480b9bc08dbe7910e3162f0d02d
Sumaia A <91845296+EngSu@users.noreply.github.com>
2026-05-30 14:13:30 -0400

    Improve memory pool allocator correctness and reuse test

--- projects/01_memory_allocator/include/memory_pool.h
+++ projects/01_memory_allocator/include/memory_pool.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #pragma once
 #include <cstddef>
 
@@ -20,3 +21,29 @@ private:
     std::size_t blockSize;
     std::size_t blockCount;
 };
+=======
+#pragma once
+#include <cstddef>
+
+class MemoryPool
+{
+public:
+    MemoryPool(std::size_t blockSize, std::size_t blockCount);
+    ~MemoryPool();
+
+    void *allocate();
+    void deallocate(void *ptr);
+
+private:
+    struct Block
+    {
+        Block *next;
+    };
+
+    Block *freeList;
+    char *pool;
+
+    std::size_t blockSize;
+    std::size_t blockCount;
+};
+>>>>>>> 7384dd9 (Improve memory pool allocator correctness and reuse test)
--- projects/01_memory_allocator/src/main.cpp
+++ projects/01_memory_allocator/src/main.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <iostream>
 #include "memory_pool.hpp"
 
@@ -16,3 +17,33 @@ int main() {
 
     return 0;
 }
+=======
+#include <iostream>
+#include "memory_pool.h"
+
+int main()
+{
+    MemoryPool pool(64, 3);
+
+    void *a = pool.allocate();
+    void *b = pool.allocate();
+    void *c = pool.allocate();
+
+    pool.deallocate(a);
+    void *d = pool.allocate(); // should reuse 'a'
+
+    std::cout << "a: " << a << "\n";
+    std::cout << "d: " << d << "\n";
+
+    if (a == d)
+    {
+        std::cout << "REUSE OK (correct free-list behavior)\n";
+    }
+    else
+    {
+        std::cout << "REUSE FAIL\n";
+    }
+
+    return 0;
+}
+>>>>>>> 7384dd9 (Improve memory pool allocator correctness and reuse test)
